c759a41d95a4926d3f588b222d486a23e30687ad
Thomas Queiroz <thomasqueirozb@gmail.com>
2020-05-01 18:28:24 -0300

    Added path class. Now screen only updated when needed

---
+++ path.js
@@ -0,0 +1,77 @@
+class Path {
+    path = [];
+    distance = 0;
+
+    clear() {
+        for (let vertex of this.path) vertex.unselect();
+        this.path = [];
+        this.distance = 0;
+    }
+
+    click(vertex) {
+        const path_last = this.path.length - 1;
+        const path_idx = this.path.indexOf(vertex);
+        if (path_idx == -1) {
+            this.push(vertex);
+        } else if (path_idx == path_last) {
+            this.pop();
+        }
+    }
+
+    push(vertex) {
+        vertex.select();
+        let last = this.path[this.path.length - 1];
+
+        if (last) {
+            // This is the first push
+            const dx = vertex.x - last.x;
+            const dy = vertex.y - last.y;
+            this.distance += Math.sqrt(dx * dx + dy * dy);
+        }
+
+        this.path.push(vertex);
+    }
+
+    pop() {
+        const vertex = this.path.pop();
+
+        let last = this.path[this.path.length - 1];
+        if (last) {
+            const dx = vertex.x - last.x;
+            const dy = vertex.y - last.y;
+            this.distance -= Math.sqrt(dx * dx + dy * dy);
+        }
+
+        vertex.unselect();
+        return vertex;
+    }
+
+    calcDist() {
+        return this.distance;
+        /*
+        let d = 0;
+        for (let i = 1; i < this.path.length; i++) {
+            const dx = this.path[i].x - this.path[i - 1].x;
+            const dy = this.path[i].y - this.path[i - 1].y;
+            d += dx * dx + dy * dy;
+        }
+        return d;
+        */
+    }
+
+    draw() {
+        const len = this.path.length;
+        for (let i = 1; i < len; i++) {
+            color(0, 255, 255);
+            strokeWeight(3);
+            stroke(125);
+
+            const x1 = this.path[i].x;
+            const y1 = this.path[i].y;
+
+            const x2 = this.path[i - 1].x;
+            const y2 = this.path[i - 1].y;
+            line(x1, y1, x2, y2);
+        }
+    }
+}
--- sketch.js
+++ sketch.js
@@ -1,61 +1,34 @@
-var path = [];
-
+var path = new Path();
 function setup() {
     createCanvas(800, 400);
     var vs = useTemplate(0);
-}
-
-function calcDist(path) {
-    let d = 0;
-    for (i = 1; i < path.length; i++) {
-        const dx = path[i].x - path[i - 1].x;
-        const dy = path[i].y - path[i - 1].y;
-        d += dx * dx + dy * dy;
-    }
-    return d;
+    updated();
 }
 
 function doubleClicked() {
-    path = [];
-    for (let vertex of vs) {
-        vertex.unselect();
-    }
+    path.clear();
+    updated();
 }
 
 function mouseClicked() {
-    const path_last = path.length - 1;
     for (let vertex of vs) {
         if (vertex.testClick(mouseX, mouseY)) {
-            const path_idx = path.indexOf(vertex);
-            if (path_idx == -1) {
-                vertex.select();
-                path.push(vertex);
-            } else if (path_idx == path_last) {
-                vertex.unselect();
-                path.pop();
-            }
+            path.click(vertex);
+            break;
         }
     }
+    updated();
 }
 
-function draw() {
+function draw() {}
+
+function updated() {
     background(0);
 
     for (let vertex of vs) vertex.update();
 
-    for (i = 1; i < path.length; i++) {
-        color(0, 255, 255);
-        strokeWeight(3);
-        stroke(125);
-
-        const x1 = path[i].x;
-        const y1 = path[i].y;
-
-        const x2 = path[i - 1].x;
-        const y2 = path[i - 1].y;
-        line(x1, y1, x2, y2);
-    }
-    const d = calcDist(path);
+    path.draw();
+    const d = (Math.round(path.calcDist() * 100) / 100).toFixed(2);
     textSize(22);
     fill(255);
     text("Dist: " + str(d), 10, 30);
